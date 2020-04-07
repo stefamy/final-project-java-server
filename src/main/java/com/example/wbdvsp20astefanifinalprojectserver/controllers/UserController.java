a89b84976c7684fa55df9708613cbcd92cb62624
Amy Stefani <amystefani1@gmail.com>
2020-04-07 12:52:31 -0700

    adding mysql to heroku instance

--- src/main/java/com/example/wbdvsp20astefanifinalprojectserver/controllers/UserController.java
+++ src/main/java/com/example/wbdvsp20astefanifinalprojectserver/controllers/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpSession;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
+@CrossOrigin(origins = "https://fierce-basin-13622.herokuapp.com", allowCredentials = "true")
 public class UserController {
 
     @Autowired
